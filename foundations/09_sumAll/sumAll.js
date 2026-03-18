9c696c510862e73516d5b7ad6479da6cbb4fc7f0
Kelvin Boakye <boakyekelvin306@gmail.com>
2026-03-17 22:25:09 -0500

    solutions to chapter 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, nums2) {
+    sum = 0;
+    mini = Math.min(num1, nums2);
+    maxi = Math.max(num1, num2);
+
+    if (mini < 0 || !Number.isInteger(mini) || !Number.isInteger(maxi)) {
+        return 'ERROR';
+    }
+
+    for (let i = mini; i <= maxi; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
