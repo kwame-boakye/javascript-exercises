5429d52f44744a55e525a33cba68bf700f98f40f
Kelvin Boakye <boakyekelvin306@gmail.com>
2026-03-17 21:53:14 -0500

    solution to chapter 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(reverse) {
+    let res = '';
+    for (let i = reverse.length - 1; i >=0; i--) {
+        res += reverse[i];
+    }
+    return res;
 };
 
 // Do not edit below this line
