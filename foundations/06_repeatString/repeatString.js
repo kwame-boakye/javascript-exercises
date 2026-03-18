394acc3f3ff4729d690278ba496753cbcd8c54ec
Kelvin Boakye <boakyekelvin306@gmail.com>
2026-03-17 21:44:34 -0500

    Added solution to chapter 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(sentence, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let res = '';
+    for (let i = 0; i < num; i++) {
+        res += sentence;
+    }
+    return res;
 };
 
 // Do not edit below this line
