200c690517a249b77c576dee36ffebbf997652e3
Kelvin Boakye <boakyekelvin306@gmail.com>
2026-03-17 22:18:30 -0500

    solution to chapter 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    const new_arr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!values.includes(arr[i])) {
+            new_arr.push(arr[i]);
+        }
+    }
+
+    return new_arr;
 };
 
 // Do not edit below this line
