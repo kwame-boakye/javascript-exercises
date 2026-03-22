7f07bf08b2f1d8819a7dd4bc7889aae10b391a94
Kelvin Boakye <boakyekelvin306@gmail.com>
2026-03-22 18:37:58 -0500

    solution to chapter 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(ages) {
+    const oldest = ages.reduce((oldest, person) => {
+        const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth;
+        const personAge = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
+        return personAge > oldestAge ? person : oldest;
+    });
+    return oldest;
 
 };
 
